966e028526e0dfb9503195d712fb55b8c39926e6
Jake Shawd <90359526+Shewwd@users.noreply.github.com>
2024-01-30 21:35:18 +0000

    pathing

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000; // or any port you prefer
+const port = 3000; 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+ res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+ console.log(`Server is running at http://localhost:${port}`);
 });
